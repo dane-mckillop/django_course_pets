7fc0e3d516df5c8e44a88422ff34620a5f2b467b
Dane Mckillop <dane.mckillop@gmail.com>
2024-05-16 18:27:02 +1000

    Complete lessons relating to Learning Django course. Re-examine main.js and applying javascript to templates

--- wisdompets/adoptions/models.py
+++ wisdompets/adoptions/models.py
@@ -7,10 +7,13 @@ class Pet(models.Model):
     species = models.CharField(max_length=30)
     breed = models.CharField(max_length=30, blank=True)
     description = models.TextField()
-    sex = models.Charfield(max_length=1, choices=SEX_CHOICES, blank=True)
+    sex = models.CharField(max_length=1, choices=SEX_CHOICES, blank=True)
     submission_date = models.DateTimeField()
     age = models.IntegerField(null=True)
     vaccinations = models.ManyToManyField('Vaccine', blank=True)
     
 class Vaccine(models.Model):
     name = models.CharField(max_length=50)
+    
+    def __str__(self):
+        return self.name
